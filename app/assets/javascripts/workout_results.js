fa63e0754bc5bdd675204a4e13236351ed088162
michael verdi <michael.v.verdi@gmail.com>
2013-11-04 09:08:20 -0500

    timer is working and updating its styling based off the success/failure of the lift

--- app/assets/javascripts/workout_results.js
+++ app/assets/javascripts/workout_results.js
@@ -3,10 +3,59 @@ $(document).ready(function() {
     $('form#new_workout_result').submit();
   });
 
+  $(".timer").hide();
+  var watch = new Stopwatch(updateDisplay, 50);
+
   $(".result-field").on("click", function(){
     decrementReps($(this));
     updateHiddenField($(this));
+    manageTimerDisplay($(this));
+    if(watchNotStarted()){
+      manageStopWatch("start", watch);
+    }
+    else{manageStopWatch("restart", watch);}
   });
+
+  $(".container").on("click", ".close", function(){
+    $(".timer").hide();
+    manageStopWatch("reset", watch);
+  });
+
+  function manageTimerDisplay(element){
+    if(liftUnsuccessful(element)){
+      $(".timer .message").text("Failing Reps is Part of the Game");
+      $(".timer .message").css("color", "#de6489" );
+      $(".timer #watch-display").css("color", "#de6489" );
+      $(".timer").css("border-color", "#de6489" );
+    }
+    else{
+      $(".timer .message").text("Woot! Way to Get Your Reps In");
+      $(".timer .message").css("color", "#7bc06e" );
+      $(".timer #watch-display").css("color", "#7bc06e" );
+      $(".timer").css("border-color", "#7bc06e" );
+    }
+  }
+
+  function liftUnsuccessful(element){
+    return element.text() != parseInt(element.data("reps"));
+  }
+
+  function watchNotStarted(){
+    return $("#watch-display").text() == ""
+  }
+
+  function clearWatchDisplay(){
+    $("#watch-display").text("");
+  }
+
+  function manageStopWatch(action, watch){
+    if(action == "start"){
+      $(".timer").show();
+      watch.start();
+    }
+    else if(action == "reset"){watch.stop(); watch.reset(); clearWatchDisplay();}
+    else if(action == "restart"){watch.restart();}
+  }
 
   function decrementReps(element){
     if( element.text() == "" ){
@@ -29,4 +78,10 @@ $(document).ready(function() {
     var liftId = element.data("lift-id");
     $("#workout_result_exercise_results_attributes_" + exerciseId + "_lift_results_attributes_" + liftId + "_reps").val(completedReps);
   }
+
+  function updateDisplay(watch){
+    var time = watch.toString();
+    $("#watch-display").text(time);
+  }
+
 });
